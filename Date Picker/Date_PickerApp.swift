b52d9adfae8f500b23ae16ec3a50c20dde1ddf5c
Daisuke Sakurai <d.sakurai0@gmail.com>
2023-09-04 13:45:02 +0900

    Initial Commit

---
+++ Date Picker/Date_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_PickerApp.swift
+//  Date Picker
+//
+//  Created by Daisuke Sakurai on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct Date_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
