3c17614238bb23e245e8e3c3545708aef5af2e23
Daisuke Sakurai <d.sakurai0@gmail.com>
2023-09-04 14:42:18 +0900

    Implement the app

--- Date Picker/ContentView.swift
+++ Date Picker/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedDate = Date()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            DatePicker("Select a Date", selection: $selectedDate, displayedComponents: [.date])
+                .datePickerStyle(DefaultDatePickerStyle())
+                .fixedSize()
+                .padding()
+            Button(action: copyDateToClipboard) {
+                Text("Copy Date to Clipboard")
+            }.padding()
+            
         }
         .padding()
+    }
+    
+    func copyDateToClipboard() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd"
+        let formattedDate = dateFormatter.string(from: selectedDate)
+        
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(formattedDate, forType: .string)
     }
 }
 
